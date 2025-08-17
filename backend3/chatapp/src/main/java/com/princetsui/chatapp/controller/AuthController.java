e3fd50dcf9b5925eb33cd44665e392fc0233f1ec
csprincedaniel <csprincedaniel@gmail.com>
2025-08-17 15:41:29 -0500

    old backend fix

--- backend3/chatapp/src/main/java/com/princetsui/chatapp/controller/AuthController.java
+++
@@ -1,8 +0,0 @@
-package com.princetsui.chatapp.controller;
-
-import com.princetsui.chatapp.model.User;
-import com.princetsui.chatapp.repository.UserRepository;
-
-public class AuthController {
-    
-}
